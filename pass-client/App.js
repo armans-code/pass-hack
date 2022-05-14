900b2bbae02b674f205d5f840e23e6f0851f9949
armans-code <arman.kumaraswamy@gmail.com>
2022-05-14 18:01:42 -0400

    added apollo provider

--- pass-client/App.js
+++ pass-client/App.js
@@ -1,24 +1,33 @@
-import "react-native-gesture-handler";
+import 'react-native-gesture-handler';
 
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { useLoadedAssets } from "./hooks/useLoadedAssets";
-import Navigation from "./navigation";
-import { useColorScheme } from "react-native";
+import { useLoadedAssets } from './hooks/useLoadedAssets';
+import Navigation from './navigation';
+import { useColorScheme } from 'react-native';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient } from '@apollo/client';
 
 export default function App() {
-  const isLoadingComplete = useLoadedAssets();
-  const colorScheme = useColorScheme();
+	const isLoadingComplete = useLoadedAssets();
+	const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+	const client = new ApolloClient({
+		uri: 'localhost:8080/graphql',
+		cache: new InMemoryCache(),
+	});
+
+	if (!isLoadingComplete) {
+		return null;
+	} else {
+		return (
+			<ApolloProvider client={client}>
+				<SafeAreaProvider>
+					<Navigation colorScheme={colorScheme} />
+					<StatusBar />
+				</SafeAreaProvider>
+			</ApolloProvider>
+		);
+	}
 }
